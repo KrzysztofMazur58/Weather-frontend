41d3722c6b5bd7d62a4e4ce595305e02bc741a7a
Krzysztof Mazur <mazurkrzysztof377@example.com~>
2025-06-19 23:26:47 +0200

    fix2

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_API_URL;
 
+console.log("BASE_URL w weatherService:", BASE_URL);
+
 export async function getForecast(lat, lon) {
   const res = await axios.get(`${BASE_URL}/daily`, {
     params: { latitude: lat, longitude: lon },
